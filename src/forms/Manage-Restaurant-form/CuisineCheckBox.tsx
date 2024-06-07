b134175471e6bd0f651b9d789e1aadf809d662d5
bhujbalpratik <pratikbhujbal743@gmail.com>
2024-06-07 20:00:59 +0530

    Manage restaurant from deploy

--- src/forms/Manage-Restaurant-form/CuisineCheckBox.tsx
+++ src/forms/Manage-Restaurant-form/CuisineCheckBox.tsx
@@ -1,7 +1,6 @@
 import { Checkbox } from "@/components/ui/checkbox"
 import { FormControl, FormItem, FormLabel } from "@/components/ui/form"
 import { ControllerRenderProps, FieldValues } from "react-hook-form"
-import { string } from "zod"
 
 type Props = {
   cuisine: string
